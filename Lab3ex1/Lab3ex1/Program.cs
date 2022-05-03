f45d2d6b8c7a71bbcecb7a7a2ad16fce028ee9ff
Misha Isopesku <isopesku05@gmail.com>
2022-05-03 11:33:57 +0300

    first commit

---
+++ Lab3ex1/Lab3ex1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("\nВиберіть варіант: \n" +
+                    "1 - Старт програми \n" +
+                    "2 - Вихід \n" +
+                    "Ваш вибір: ");
+
+            int menu = int.Parse(Console.ReadLine());
+            if (menu == 1)
+            {
+                Console.Write("Введіть рядок: ");
+                String str = Console.ReadLine();
+                int indexOfColon = str.IndexOf(':');
+                if (indexOfColon != -1)
+                {
+                    String resultstr = str.Substring(indexOfColon + 1);
+                    Console.WriteLine("Символи після ':' - " + resultstr);
+                }
+                else
+                {
+                    Console.WriteLine("Символи після ':' - ");
+                }
+
+                String[] sentences = str.Split(".");
+
+                int result = 0;
+                foreach (String sentence in sentences)
+                {
+                    String[] words = sentence.Trim().Split(" ");
+                    if (words.Length % 2 != 0)
+                    {
+                        result++;
+                    }
+                }
+
+                Console.WriteLine("Кількість речень, що містять непарну кількість слів - " + result);
+
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ Lab3ex1/Lab3ex2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("\nВиберіть варіант: \n" +
+                    "1 - Старт програми \n" +
+                    "2 - Вихід \n" +
+                    "Ваш вибір: ");
+        string s = Console.ReadLine();
+
+
+        switch (s)
+        {
+         case "1":
+                int[] array = Array.ConvertAll(File.ReadAllLines("C:\\1.txt"), e => e.Length);
+
+                {
+                    for (int i = 0; i < array.Length; Console.WriteLine($"Рядок {i + 1}, кількість символів: {array[i++]}")) ;
+                }
+                break;
+
+
+         case "2":
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Программа завершила роботу");
+                Console.ResetColor();
+                break;
+        }
+    }
+}
